35919533b09a008ad1da7d8f531182f87e6c7fce
ikeda <ikeda@IKEDA-MBA.local>
2017-06-09 15:45:16 +0200

    Modify fc_reducer.py to accept arbitrary FORCE_CONSTANTS name to be written

--- ph_analysis/fc/fc_reducer.py
+++ ph_analysis/fc/fc_reducer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 import argparse
 import numpy as np
-from phonopy.file_IO import parse_FORCE_CONSTANTS
+from phonopy.file_IO import parse_FORCE_CONSTANTS, write_FORCE_CONSTANTS
 from vasp.poscar import Poscar
 from ph_analysis.analysis.fc_analyzer_base import FCAnalyzerBase
 
-
-__author__ = "Yuji Ikeda"
+__author__ = 'Yuji Ikeda'
 
 
 class FCReducer(object):
@@ -17,6 +17,7 @@ class FCReducer(object):
                  fc_filename="FORCE_CONSTANTS"):
         self._poscar = Poscar(poscar_filename)
         self._force_constants = parse_FORCE_CONSTANTS(fc_filename)
+        self._fc_reduced = None
 
     def find_indices_from_positions(self, positions):
         """Find atomic indices for removed FCs"""
@@ -34,23 +35,19 @@ class FCReducer(object):
 
         return indices_all
 
-    def do_postprocess(self, force_constants):
+    def write(self, filename):
+        write_FORCE_CONSTANTS(self._fc_reduced, filename=filename)
+
+    def do_postprocess(self):
         poscar = self._poscar
 
         fc_analyzer = FCAnalyzerBase(
             atoms=poscar.get_atoms(),
-            force_constants=force_constants,
+            force_constants=self._fc_reduced,
             is_symmetrized=False,
         )
-    
-        fc_analyzer.check_translational_invariance()
-        fc_analyzer.write_force_constants("FORCE_CONSTANTS_reduced")
-    
-        print("=" * 80)
-    
         fc_analyzer.force_translational_invariance()
-        fc_analyzer.check_translational_invariance()
-        fc_analyzer.write_force_constants("FORCE_CONSTANTS_reduced_TI")
+        self._fc_reduced = fc_analyzer.get_force_constants()
 
     def keep_FCs_for_positions(self, positions):
         """
@@ -80,7 +77,8 @@ class FCReducer(object):
                 force_constants[i0, i1] = force_constants_orig[i0, i1]
                 force_constants[i1, i0] = force_constants_orig[i1, i0]
 
-        self.do_postprocess(force_constants)
+        self._fc_reduced = force_constants
+        self.do_postprocess()
 
     def remove_FCs_for_positions(self, positions):
         """
@@ -109,23 +107,25 @@ class FCReducer(object):
                 force_constants[i0, i1] = 0.0
                 force_constants[i1, i0] = 0.0
 
-        self.do_postprocess(force_constants)
+        self._fc_reduced = force_constants
+        self.do_postprocess()
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--poscar_filename",
+    parser.add_argument('-p', '--poscar',
                         default="POSCAR",
                         type=str,
                         help="Filename of POSCAR.")
-    parser.add_argument("--fc_filename",
+    parser.add_argument("--fc",
                         default="FORCE_CONSTANTS",
                         type=str,
                         help="Filename of FORCE_CONSTANTS.")
     args = parser.parse_args()
     FCReducer(
-        poscar_filename=args.poscar_filename,
-        fc_filename=args.fc_filename,
-    ).run()
+        poscar_filename=args.poscar,
+        fc_filename=args.fc,
+    )
 
 if __name__ == "__main__":
     main()
