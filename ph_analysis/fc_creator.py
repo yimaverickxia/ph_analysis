08b05f6cf7f19fb6af8c3530d2e2db7e906ebc2b
ikeda <ikeda@IKEDA-MBA.local>
2017-07-15 07:11:54 +0200

    Modify to use PoscarIdealCreator

--- ph_analysis/fc_creator.py
+++ ph_analysis/fc_creator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-import os
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 import shutil
 import subprocess
-from vasp.poscar import Poscar
-from autotools import symlink_force
+
+__author__ = 'Yuji Ikeda'
 
 
 class FCCreator(object):
@@ -14,45 +14,32 @@ class FCCreator(object):
             directory_data="./",
             poscar_filename="POSCAR",
             positions_1nn=None):
-        self._phonopy = subprocess.check_output(["which", "phonopy"]).strip()
         self._directory_data = directory_data
         self._poscar_filename = poscar_filename
         self._positions_1nn = positions_1nn
 
-        print("phonopy_path:", self._phonopy)
         print("directory_data:", self._directory_data)
         print("poscar_filename:", self._poscar_filename)
 
     def run(self):
+        self.copy_files()
         self.create_poscar_ideal()
         self.create_force_constants()
         # self.reduce_force_constants()
 
-    def create_poscar_ideal(self):
-        """
-
-        For disordered systems with relaxed atomic positions,
-        it might be better to use initial atomic positions to use the
-        symmetry of the structure (POSCAR_initial).
-        """
-        from phonopy.structure.atoms import symbol_map
-        symbol_map['X'] = 0
+    def copy_files(self):
         directory_data = self._directory_data
-        symlink_force(directory_data + self._poscar_filename, "POSCAR")
-        poscar = Poscar("POSCAR")
-        number_of_atoms = poscar.get_atoms().get_number_of_atoms()
-        dummy_symbols = ["X"] * number_of_atoms
-        poscar.get_atoms().set_chemical_symbols(dummy_symbols)
-        poscar.write_poscar("POSCAR_ideal")
+        shutil.copy2(directory_data + '/POSCAR', '.')
+        shutil.copy2(directory_data + '/FORCE_SETS', '.')
+        shutil.copy2(directory_data + '/writefc.conf', '.')
+
+    def create_poscar_ideal(self):
+        from .poscar_ideal_creator import PoscarIdealCreator
+        PoscarIdealCreator().run()
 
     def create_force_constants(self):
-        directory_data = self._directory_data
-        phonopy = self._phonopy
-        symlink_force(directory_data + "/FORCE_SETS", "FORCE_SETS")
-        symlink_force(directory_data + "/writefc.conf", "writefc.conf")
-        # Create FORCE_CONSTANTS
-        with open('phonopy_writefc_conf.log', 'w') as f:
-            subprocess.call([phonopy, 'writefc.conf', '-v'], stdout=f)
+        with open('writefc_conf.log', 'w') as f:
+            subprocess.call(['phonopy', 'writefc.conf', '-v'], stdout=f)
 
     def reduce_force_constants(self):
         from fc_reducer.fc_reducer import FCReducer
