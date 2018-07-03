c1759ad48569efa72483a4b67941dfd5f2773184
ikeda <ikeda@IKEDA-MBA.local>
2018-07-03 16:09:58 +0200

    Update how to give dummy symbols

--- ph_analysis/poscar_ideal_creator.py
+++ ph_analysis/poscar_ideal_creator.py
@@ -8,18 +8,21 @@ __author__ = 'Yuji Ikeda'
 
 
 class PoscarIdealCreator(object):
-    @staticmethod
-    def run():
+    def run(self):
         """
 
         For disordered systems with relaxed atomic positions,
         it might be better to use initial atomic positions to use the
         symmetry of the structure (POSCAR_initial).
         """
-        from phonopy.structure.atoms import symbol_map
-        symbol_map['X'] = 0
         poscar = Poscar('POSCAR')
         number_of_atoms = poscar.get_atoms().get_number_of_atoms()
-        dummy_symbols = ['X'] * number_of_atoms
+        dummy_symbols = self.create_dummy_symbols(number_of_atoms)
         poscar.get_atoms().set_chemical_symbols(dummy_symbols)
         poscar.write_poscar('POSCAR_ideal')
+
+    @staticmethod
+    def create_dummy_symbols(number_of_atoms):
+        from phonopy.structure.atoms import symbol_map
+        symbol_map['X'] = 0
+        return ['X'] * number_of_atoms
